cd4e7c7a41a614315f5920333f679779eea9e36d
ying-611 <71594654+ying-611@users.noreply.github.com>
2024-03-21 11:26:15 +0800

    Delete dist directory

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Button.css";
-export interface ButtonProps {
-    label: string;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-export default Button;
