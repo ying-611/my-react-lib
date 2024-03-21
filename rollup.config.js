24fbb19e8647d890a7cef276fec9d7619b4e5cc0
廖盈盈 <liaoyyb@digitalchina.com>
2024-03-21 13:58:30 +0800

    📦 NEW: 添加测试

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
 import postcss from "rollup-plugin-postcss";
+
+//NEW
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -23,10 +26,14 @@ export default [
       },
     ],
     plugins: [
+      // NEW
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      // NEW
+      terser(),
     ],
   },
   {
